c81688814eb69f874ce56b4ebcca55204d0f02ce
Just <37525404+justofisker@users.noreply.github.com>
2024-06-23 15:38:52 -0700

    Fix outline for sprite browser area

--- godot/rotmg_atlases_dll.gd
+++ godot/rotmg_atlases_dll.gd
@@ -1,12 +1,15 @@
 extends SpriteSheetDeserializer
  
-func get_animated_sprite_textures_export(sprite_sheet: String, index: int) -> Array[Texture2D]:
+func get_animated_sprite_textures_export(sprite_sheet: String, index: int, default: bool = false) -> Array[Texture2D]:
 	var frames = get_animated_sprite_textures(sprite_sheet, index)
 	
 	var out : Array[Texture2D]
 	
 	var outline_size := GlobalSettings.export_outline_size
 	var export_scale := GlobalSettings.export_scale
+	if default:
+		outline_size = 1
+		export_scale = 5
 	
 	for frame in frames:
 		var tex : AtlasTexture = frame.duplicate()
@@ -19,10 +22,13 @@ func get_animated_sprite_textures_export(sprite_sheet: String, index: int) -> Ar
 	
 	return out
 
-func get_texture_export(sprite_sheet: String, index: int) -> AtlasTexture:
+func get_texture_export(sprite_sheet: String, index: int, default: bool = false) -> AtlasTexture:
 	var tex : AtlasTexture = get_texture(sprite_sheet, index)
 	var outline_size := GlobalSettings.export_outline_size
 	var export_scale := GlobalSettings.export_scale
+	if default:
+		outline_size = 1
+		export_scale = 5
 	if outline_size != 0:
 		tex.region.position.x -= ceilf(float(outline_size) / export_scale)
 		tex.region.position.y -= ceilf(float(outline_size) / export_scale)
